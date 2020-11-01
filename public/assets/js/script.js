f6a838495b721c51e4ff5ed25ff165004358eca1
mercspring <68706508+mercspring@users.noreply.github.com>
2020-11-01 11:40:18 -0800

    added dynamic updating of cardio and lifting

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,8 +5,8 @@
 //Handler for the add exercise button
 $(".add-exercise-btn").on("click", function(event){
     event.preventDefault()
-//     const typeSelector = $("<select id='type'>")
-//     typeSelector.append("<option value='Lifting'>Lifting</option> <option value='Cardio'>Cardio</option> <option value='Other'>Other</option>")
+    const typeSelector = $("<select id='type'>")
+    typeSelector.append("<option value='Lifting'>Lifting</option> <option value='Cardio'>Cardio</option> <option value='Other'>Other</option>")
 
 // console.log("clicked")
     $(this).parent().parent().append(typeSelector);
@@ -18,17 +18,17 @@ $(".add-exercise-btn").on("click", function(event){
 $(document).on("change", "#type", function(event){
     console.log("selected")
 if($(this).val() === "Cardio"){
-    $("#add-exercise").clear();    
-    $("#add-exercise").append("<label for='exercise-name'> Name:</label><input type='text' id='exercise-name'>")
-    $("#add-exercise").append("<label for='exercise-duration'> Name:</label><input type='text' id='exercise-name'>")
-    $("#add-exercise").append("<label for='exercise-distance'> Name:</label><input type='text' id='exercise-name'>")
+    $(this).parent().remove("input[type=text]")
+    $(this).parent().append("<label for='exercise-name'> Name:</label><input type='text' class='exercise-name'>")
+    $(this).parent().append("<label for='exercise-duration'> Duration:</label><input type='text' class='exercise-duration'>")
+    $(this).parent().append("<label for='exercise-distance'> Distance:</label><input type='text' class='exercise-distance'>")
 } else if($(this).val() === "Lifting"){
-    $("#add-exercise").clear();    
-    $("#add-exercise").append("<label for='exercise-name'> Name:</label><input type='text' id='exercise-name'>")
-    $("#add-exercise").append("<label for='exercise-weight'> Weight:</label><input type='text' id='exercise-name'>")
-    $("#add-exercise").append("<label for='exercise-reps'> Reps:</label><input type='text' id='exercise-name'>")
-    $("#add-exercise").append("<label for='exercise-sets'> Sets:</label><input type='text' id='exercise-name'>")
-    $("#add-exercise").append("<label for='exercise-duration'> Duration:</label><input type='text' id='exercise-name'>")
+    $(this).parent().remove("input[type=text]");    
+    $(this).parent().append("<label for='exercise-name'> Name:</label><input type='text' class='exercise-name'>")
+    $(this).parent().append("<label for='exercise-weight'> Weight:</label><input type='text' class='exercise-weight'>")
+    $(this).parent().append("<label for='exercise-reps'> Reps:</label><input type='text' class='exercise-reps'>")
+    $(this).parent().append("<label for='exercise-sets'> Sets:</label><input type='text' class='exercise-sets'>")
+    $(this).parent().append("<label for='exercise-duration'> Duration:</label><input type='text' class='exercise-duration'>")
 }
 })
 // $.ajax({
