90e61f43c941f2101a37bf9674db992cfcd8d224
mercspring <68706508+mercspring@users.noreply.github.com>
2020-11-02 20:26:47 -0800

    added deploying information

--- server.js
+++ server.js
@@ -20,7 +20,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 // db.Workout.create({ name: "Crashing through the forest" })
 //   .then(result => {
@@ -29,11 +34,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", { 
 //   .catch(({ message }) => {
 //     console.log(message);
 //   });
-  // Import routes and give the server access to them.
-  const frontendRoutes = require("./controllers/frontendController");
-  app.use(frontendRoutes)
-  const apiRoutes = require("./controllers/apiController");
-  app.use('/api/', apiRoutes)
+// Import routes and give the server access to them.
+const frontendRoutes = require("./controllers/frontendController");
+app.use(frontendRoutes)
+const apiRoutes = require("./controllers/apiController");
+app.use('/api/', apiRoutes)
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
